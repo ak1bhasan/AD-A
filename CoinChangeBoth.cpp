974f1b24c7473dfa28f612054acf2252309b63c1
Akib Hasan <136964348+ak1bhasan@users.noreply.github.com>
2025-03-04 21:41:58 +0600

    Add files via upload

---
+++ CoinChangeBoth.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <algorithm> // For min function
+using namespace std;
+
+// Coin Change - Number of Ways
+int coinChangeNumWays(int coins[], int n, int amount)
+{
+    int dp[n+1][amount+1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= amount; j++)
+        {
+            if (i == 0) dp[i][j] = 0;
+            if (j == 0) dp[i][j] = 1;
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= amount; j++)
+        {
+            dp[i][j] = dp[i-1][j];
+            if (j >= coins[i-1])
+            {
+                dp[i][j] += dp[i][j - coins[i-1]];
+            }
+        }
+    }
+    return dp[n][amount];
+}
+
+// Coin Change - Minimum Number of Coins
+int coinChangeMinCoins(int coins[], int n, int amount)
+{
+    int INF = 1e9;
+    int dp[n+1][amount+1];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= amount; j++)
+        {
+            if (i == 0) dp[i][j] = INF;
+            if (j == 0) dp[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= amount; j++)
+        {
+            dp[i][j] = dp[i-1][j];
+            if (j >= coins[i-1])
+            {
+                dp[i][j] = min(dp[i][j], dp[i][j - coins[i-1]] + 1);
+            }
+        }
+    }
+    return dp[n][amount] == INF ? -1 : dp[n][amount];
+}
+
+int main()
+{
+    int coins[] = {3, 8, 9};
+    int n = sizeof(coins) / sizeof(coins[0]);
+    int amount = 11;
+
+    cout << "Coin Change - Number of Ways: " << coinChangeNumWays(coins, n, amount) << endl;
+    cout << "Coin Change - Minimum Number of Coins: " << coinChangeMinCoins(coins, n, amount) << endl;
+
+    return 0;
+}
--- LCS.cpp
+++ LCS.cpp
@@ -1,55 +1,45 @@
-//#include<bits/stdc++.h>
-//using namespace std;
-//int lcs(string s1, string s2)
-//{
-//    int n = s1.length();
-//    int m = s2.length();
-//
-//    int dp[n + 1][m + 1];
-//
-//    for(int i = 0; i <= n; i++)
-//    {
-//        for(int j = 0; j <= m; j++)
-//        {
-//            if(i == 0 || j == 0)
-//            {
-//                dp[i][j] = 0;
-//            }
-//
-//            else
-//            {
-//                if(s1[i] == s2[j])
-//                {
-//                    dp[i][j] = dp[i - 1][j - 1] + 1;
-//                }
-//                else
-//                {
-//                    dp[i][j] = max(dp[i][j - 1],dp[i - 1][j]);
-//                }
-//            }
-//        }
-//    }
-//    return dp[n][m];
-//}
-//
-//int main()
-//{
-//    string s1;
-//    cin >> s1;
-//    string s2;
-//    cin >> s2;
-//    cout << lcs(s1, s2) << endl;
-//}
-
-
 #include<bits/stdc++.h>
 using namespace std;
+int lcs(string s1, string s2)
+{
+    int n = s1.length();
+    int m = s2.length();
+
+    int dp[n + 1][m + 1];
+
+    for(int i = 0; i <= n; i++)
+    {
+        for(int j = 0; j <= m; j++)
+        {
+            if(i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+
+            else
+            {
+                if(s1[i] == s2[j])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i][j - 1],dp[i - 1][j]);
+                }
+            }
+        }
+    }
+    return dp[n][m];
+}
 
 int main()
 {
-    int n,m;
-    cin >> n >> m;
-    cout << n * m / 2 << endl;
-
-    return 0;
+    string s1;
+    cin >> s1;
+    string s2;
+    cin >> s2;
+    cout << lcs(s1, s2) << endl;
 }
+
+
+
